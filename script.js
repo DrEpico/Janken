c7eecd68b6e9431613549aaf1c3f1f0d446c07f4
ali <adadashipour@gmail.com>
2022-11-02 13:28:11 +0000

    [200~Add if statements. Now the function will return either rock, paper or scissors based on the number that getComputerChoice() returns. (0=rock, 1=paper, 2=scissors)

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // a fucntion that randomely return either "rock", "paper" or "scissors"
-    // a function that calls the random number function which returns a number out 0 to 1. Multiplying the retunerned number by 3 will give numbers between 0, 1 or 2. 
+    // a function that calls the random number function which returns a number netween 0 to 1. Multiplying the retunerned number by 3 will give numbers between 0, 1 or 2.
+    // the function will return either "rock", "paper" or "scissors" based on the number that getComputerChoice() returns. (0=rock, 1=paper, 2=scissors) 
 function getComputerChoice(){ 
     let randomNumber = Math.floor(Math.random() * 3);
-    return randomNumber
+    //console.log(randomNumber);
+    if (randomNumber == 0){
+        return "rock"
+    } else if (randomNumber == 1){
+        return "paper"
+    } else if (randomNumber == 2){
+        return "scissors"
+    }   
 }
 console.log(getComputerChoice());
