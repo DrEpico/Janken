45a6a93dffc06b82c2f652990ecfb4f94dfb0d3b
ali <adadashipour@gmail.com>
2022-11-08 21:07:25 +0000

    Move getComputerChoice(), randomNumber, playerSelection, computerChoice variables to get them in a loop (which does work). The Janken function now does 'ask for input => return the result' for 5 times (as alert/prompt).

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // a fucntion that randomely return either "rock", "paper" or "scissors"
     // a function that calls the random number function which returns a number netween 0 to 1. Multiplying the retunerned number by 3 will give numbers between 0, 1 or 2.
     // the function will return either "rock", "paper" or "scissors" based on the number that getComputerChoice() returns. (0=rock, 1=paper, 2=scissors) 
-function getComputerChoice(){ 
-    let randomNumber = Math.floor(Math.random() * 3);
-    //console.log(randomNumber);
-    if (randomNumber == 0){
-        return "rock"
-    } else if (randomNumber == 1){
-        return "paper"
-    } else if (randomNumber == 2){
-        return "scissors"
-    }   
-}
+//function getComputerChoice(){ 
+    //let randomNumber = Math.floor(Math.random() * 3);
+    ///console.log(randomNumber);
+    //if (randomNumber == 0){
+    //    return "rock"
+    //} else if (randomNumber == 1){
+    //    return "paper"
+    //} else if (randomNumber == 2){
+    //    return "scissors"
+    //}   
+//}
 //Result of the getComputerChoice function is now stored as a constant which means that the playRound funtion now has only one hand from the getComputerChoice function 
 //to compare with the user input.
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
+//let computerChoice = getComputerChoice();
+//console.log(computerChoice);
 
 //OVERVIEW OF THE END PRODUCT
 //a function that plays a single round of Rock Paper Scissors. 
@@ -23,39 +23,54 @@ console.log(computerChoice);
         //playerSelection could be taken from a prompt (case insensitive).
     //then returns a string that declares the winner of the round. 
 
-let playerSelection = prompt("Choose a hand by typing Rock, Paper or Scissors").toLowerCase()
-console.log(playerSelection);
-
 function playRound(){
+    function getComputerChoice(){ 
+        let randomNumber = Math.floor(Math.random() * 3);
+        //console.log(randomNumber);
+        if (randomNumber == 0){
+            return "rock"
+        } else if (randomNumber == 1){
+            return "paper"
+        } else if (randomNumber == 2){
+            return "scissors"
+        }
+    }
+
+    let playerSelection = prompt("Choose a hand by typing Rock, Paper or Scissors").toLowerCase()
+    console.log(playerSelection);
+
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    
     if (computerChoice == "rock" && playerSelection == "paper"){
         return "Ayo dam, u win." 
-    } else if (computerChoice == "rock" && playerSelection == "scissors"){
+    } else if (computerChoice === "rock" && playerSelection === "scissors"){
         return "What a nub, u lose."
-    } else if (computerChoice == "rock" && playerSelection == "rock"){
+    } else if (computerChoice === "rock" && playerSelection === "rock"){
         return "Boring. Draw game."
-    } else if (computerChoice == "paper" && playerSelection == "scissors"){
+    } else if (computerChoice === "paper" && playerSelection === "scissors"){
         return "Ayo dam, u win."
-    } else if (computerChoice == "paper" && playerSelection == "rock"){
+    } else if (computerChoice === "paper" && playerSelection === "rock"){
         return "What a nub, u lose."
-    } else if (computerChoice == "paper" && playerSelection == "paper"){
+    } else if (computerChoice === "paper" && playerSelection === "paper"){
         return "Boring. Draw game."
-    } else if (computerChoice == "scissors" && playerSelection == "rock"){
+    } else if (computerChoice === "scissors" && playerSelection === "rock"){
         return "Ayo dam, u win."
-    } else if (computerChoice == "scissors" && playerSelection == "paper"){
+    } else if (computerChoice === "scissors" && playerSelection === "paper"){
         return "What a nub, u lose."
-    } else if (computerChoice == "scissors" && playerSelection == "scissors"){
+    } else if (computerChoice === "scissors" && playerSelection === "scissors"){
         return "Boring. Draw game."
     } else return "Wrong input, which means trains are better."    
 }
-alert(playRound());
+//alert(playRound());
 
 //Janken function calls the playRound function inside of it 5 times to play a 5 round game that keeps score and reports a winner at the end.
     //why does the janken function stop after executing one loop out of 5 => because after i leave the fucntion the rest of the for loop is not executed. 
     //to execute the all 5, the for loop needs to be "somewhere else".
-    function Janken(){
-        for (let i = 0; i < 5; i++){
-            console.log(playRound(i));
-            }       
-    }
-    console.log(Janken());
-    //alert(Janken());
+function Janken(){
+    for (let i = 0; i <= 4; i++){
+        alert(playRound());
+        }       
+}
+console.log(Janken());
+//alert(Janken());
