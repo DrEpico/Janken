7efa2b0558690478f664a66ca9bbb4ff1cc65460
ali <adadashipour@gmail.com>
2022-11-09 20:08:55 +0000

    Change console.log method to alert method for announcing the winner. Change player/computerScore variables default number to 1 to ensure correct number is shown to the user on prompt and as an exchange, -1 the value of console.log at the end to sync the values. getFinalResult() was moved to inside of the playRound() to ensure it is only ran once, at the end of whole process Edit and reword comments.

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
-// a fucntion that randomely return either "rock", "paper" or "scissors"
-    // a function that calls the random number function which returns a number netween 0 to 1. Multiplying the retunerned number by 3 will give numbers between 0, 1 or 2.
-    // the function will return either "rock", "paper" or "scissors" based on the number that getComputerChoice() returns. (0=rock, 1=paper, 2=scissors) 
-
-//function getComputerChoice(){ 
-    //let randomNumber = Math.floor(Math.random() * 3);
-    ///console.log(randomNumber);
-    //if (randomNumber == 0){
-    //    return "rock"
-    //} else if (randomNumber == 1){
-    //    return "paper"
-    //} else if (randomNumber == 2){
-    //    return "scissors"
-    //}   
-//}
-
-//------------------------------------------------------------------------------ABANDONED-----------------------------------------------------------------------------
-
-//Result of the getComputerChoice function is now stored as a constant which means that the playRound funtion now has only one hand from the getComputerChoice function 
-//to compare with the user input.
-//let computerChoice = getComputerChoice();
-//console.log(computerChoice);
-
-//OVERVIEW OF THE END PRODUCT
+//OVERVIEW OF THE EXPECTED END PRODUCT
 //a function that plays a single round of Rock Paper Scissors. 
-    //the function takes two parameters of playerSelection and computerSelection.--
+    //the function takes two parameters of playerSelection and computerSelection.
         //playerSelection could be taken from a prompt (case insensitive).
     //then returns a string that declares the winner of the round. 
 
+// scores 
 let playerScore = 0;
 
 let computerScore = 0;
 
+// a fucntion that randomely return either "rock", "paper" or "scissors"
+    // a function that calls the random number function which returns a number netween 0 to 1. Multiplying the retunerned number by 3 will give numbers between 0, 1 or 2.
+    // the function will return either "rock", "paper" or "scissors" based on the number that getComputerChoice() returns. (0=rock, 1=paper, 2=scissors) 
 function playRound(){
     function getComputerChoice(){ 
         let randomNumber = Math.floor(Math.random() * 3);
-        //console.log(randomNumber);
+        //console.log(randomNumber); //for debugging
         if (randomNumber == 0){
             return "rock"
         } else if (randomNumber == 1){
@@ -45,10 +26,11 @@ function playRound(){
     }
 
     let playerSelection = prompt("Choose a hand by typing Rock, Paper or Scissors. First to 3 wins!").toLowerCase()
-    console.log(playerSelection);
+    console.log(playerSelection); //for debugging
 
+    // Result of the getComputerChoice function is stored as a variable and not a constant (to avoid the "vs the same computerChoice for 5 rounds" issue).
     let computerChoice = getComputerChoice();
-    console.log(computerChoice);
+    console.log(computerChoice); //for debugging
     
     if (computerChoice == "rock" && playerSelection == "paper"){
         return "Ayo dam, u win.\nScore: " + playerScore++;
@@ -70,7 +52,6 @@ function playRound(){
         return "Boring. Draw game." //TODO:
     } else return "What a nub, u lose.\nScore: " + computerScore++;    
 }
-//alert(playRound());
 
 //Janken function calls the playRound function inside of it 5 times to play a 5 round game that keeps score and reports a winner at the end.
     //why does the janken function stop after executing one loop out of 5 => because after i leave the fucntion the rest of the for loop is not executed. 
@@ -82,16 +63,17 @@ function Janken(){
 }
 
 function getFinalResult(){
-    if (playerScore = computerScore){
+    if (playerScore == computerScore){
         return "DRAW"
-    } else if (computerScore <= 3){
+    } else if (computerScore >> playerScore){
         return "You Lose..."
-    } else if (playerScore <= 3){
+    } else if (playerScore >> computerScore){
         return "YOU WIN!!!"
     }
 }
+alert(getFinalResult());
+
+//for debugging ------------------------------
 console.log(Janken());
 console.log("player: " + playerScore);
 console.log("computer: " + computerScore);
-console.log(getFinalResult());
-//alert(Janken());
